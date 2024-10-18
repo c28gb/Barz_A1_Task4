69642ff01e486395b1e7640429a268e0b3d2c1e2
Graydon Barz <c28gb@dalton.org>
2024-10-18 11:47:39 -0400

    Added more comments, split_doubles function, and print_ciphertext function

--- Barz_A1_Task4.py
+++ Barz_A1_Task4.py
@@ -1,28 +1,31 @@
 uppercase = 'ABCDEFGHIKLMNOPQRSTUVWXYZ'
 #Things to do: 
-# - make the split text work for spaces as well
+# - decryption method
+# - clean up everything
 
-
+#removes double occurances of a letter in a string (used for keywords and table creation)
 def remove_duplicates(string):
     t = ""
     for i in string:
         if i in t:
             pass
         else:
-            t = t + i
-            
+            t = t + i 
     return t
 
-def remove_doubles(string):
+#adds an 'X' inbetween double occurances of letters (hello -> helxlo)
+def split_doubles(string):
+    new_string = ''
     for i in range (len(string)-1):
         if string[i] == string[i+1]:
-            string = string[:i] + 'x' + string[i:]
-    return string
+            new_string += string[i] + 'X'
+        else:
+            new_string += string[i]
+    return new_string + string[-1]
 
 #split text into groups of two, adds x after if the # of letters is odd
 def split_text(text):
-    text = text.upper().replace(' ','')
-    text = text.replace('J','I')
+    text = split_doubles(text.upper().replace(' ','').replace('J','I'))
     new_text = [list(text[letter:letter+2]) for letter in range(0,len(text),2)]
 
     for group in new_text:
@@ -32,15 +35,16 @@ def split_text(text):
 
 #creates a key square based on keyword
 def create_key_square(key):
-    key = key.replace('J','I')
+    key = key.replace('J','I').upper()
     table = [[],[],[],[],[]]
     current_row = 0
 
     for letter in remove_duplicates(key): 
+        if len(table[current_row]) == 5:
+            current_row += 1
         table[current_row].append(letter)
     
     for letter in uppercase:
-
         if len(table[current_row]) == 5:
             current_row += 1
             if current_row == 5:
@@ -63,6 +67,7 @@ def encrypt_pair(table,pair):
     else: 
         return [table[letter1[0]][letter2[1]],table[letter2[0]][letter1[1]]]
 
+#goes through each pair and encrypts it
 def encrypt_text(table,text):
     cipher_text = []
     for pair in text:
@@ -75,15 +80,19 @@ def get_coord(table,letter):
         for column in row:
             if letter == column: 
                 return [table.index(row),table[table.index(row)].index(column)]
-         
-#print(split_text('Johnny'))
-table = create_key_square('HELLO') 
-#print(table)  
-#print(remove_duplicates('Hello world'))   
-#print(get_coord(table,'Z'))
-#print(encrypt_pair(table,['H','D']))
 
-#print(create_key_square('IOHN'))
-print(split_text('plain exts'))
-#print(encrypt_text(table,split_text('plaintexts')))
-print(remove_doubles('hello'))
+#formats ciphertext in str format
+def print_ciphertext(ciphertext):      
+    text = ''
+    for pair in ciphertext:
+        for letter in pair: 
+            text += letter
+        text += ' '
+    return text      
+
+keyword = input('Keyword: ')
+plaintext = input('Plaintext: ')
+table = create_key_square(keyword)
+ciphertext = encrypt_text(table,split_text(split_doubles(plaintext)))
+print(split_text(plaintext))
+print(print_ciphertext(ciphertext))
